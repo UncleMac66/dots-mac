f6f37493eb5be821aa377290c036ef4b2f75f9c2
unclemac66 <macstephen66@gmail.com>
2026-01-11 11:52:20 -0500

    Sun Jan 11 11:52:20 AM EST 2026 - updates

--- dots-bin/updatedots.sh
+++ dots-bin/updatedots.sh
@@ -22,12 +22,12 @@ echo -e "Attempting to stow changes..."
 echo -e "----------\n"
 echo -e "Backing up some files req'd for xfce de..."
 
-# if [ -f ~/.config/xfce4/xfconf/xfce-perchannel-xml/ ]; then
+if [ -d ~/.config/xfce4/xfconf/xfce-perchannel-xml/ ]; then
   for i in $(cat ~/dots-mac/.config/xfce4/xfconf/xfce-perchannel-xml/files); do
   	cp -v  ~/.config/xfce4/xfconf/xfce-perchannel-xml/$i ~/.config/xfce4/xfconf/xfce-perchannel-xml/$i.bak.$(date -I)
   	mv -v  ~/.config/xfce4/xfconf/xfce-perchannel-xml/$i ~/dots-mac/.config/xfce4/xfconf/xfce-perchannel-xml/$i
   done
-# fi
+fi
 
 stow --ignore .gitignore . && echo -e "Success!"
 if [[ "$?" -gt 0 ]]; then
